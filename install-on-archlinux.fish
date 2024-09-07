d356602835171bed267e2069c9b30b39a86de70f
Thomas Fritz <thomas.fritz.2000@gmx.de>
2024-09-08 01:06:09 +0200

    Add missing arch dependencies

--- install-on-archlinux.fish
+++ install-on-archlinux.fish
@@ -1,3 +1,3 @@
 # this install script probably doesn't work but whatever, I'll fix it when I need it
-paru -S hyprland waybar pavucontrol alacritty archlinux-xdg-menu waypaper polkit-kde-agent swayosd-git pamac
+paru -S hyprland waybar pavucontrol alacritty archlinux-xdg-menu waypaper polkit-kde-agent swayosd-git pamac pywal wofi
 ./install-config.fish
