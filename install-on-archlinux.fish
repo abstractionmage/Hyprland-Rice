87254142b6fdc93e2f3b0eb6d11ea195bc099213
Thomas Fritz <thomas.fritz.2000@gmx.de>
2024-09-08 01:20:09 +0200

    Add night light

--- install-on-archlinux.fish
+++ install-on-archlinux.fish
@@ -1,3 +1,3 @@
 # this install script probably doesn't work but whatever, I'll fix it when I need it
-paru -S hyprland waybar pavucontrol alacritty archlinux-xdg-menu waypaper polkit-kde-agent swayosd-git pamac pywal wofi
+paru -S hyprland waybar pavucontrol alacritty archlinux-xdg-menu waypaper polkit-kde-agent swayosd-git pamac pywal wofi hyprshade
 ./install-config.fish
