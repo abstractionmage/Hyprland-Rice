8bc941245db301c70591d65acbca3f310e3d68b8
Thomas Fritz <thomas.fritz.2000@gmx.de>
2024-09-08 02:45:21 +0200

    Fix wrong arch dependency

--- install-on-archlinux.fish
+++ install-on-archlinux.fish
@@ -1,3 +1,3 @@
 # this install script probably doesn't work but whatever, I'll fix it when I need it
-paru -S hyprland waybar pavucontrol alacritty archlinux-xdg-menu waypaper polkit-kde-agent swayosd-git pamac pywal wofi hyprshade
+paru -S hyprland waybar pavucontrol alacritty archlinux-xdg-menu waypaper polkit-kde-agent swayosd-git pamac wofi hyprshade pywal-16-colors
 ./install-config.fish
